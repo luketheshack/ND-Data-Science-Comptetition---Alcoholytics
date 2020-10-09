3cb6375ce455005c4c999e8b93c4993181f4d01a
Aeolus1364 <millerluke1364@gmail.com>
2020-10-09 11:30:42 -0400

    added flip.py to convert from nominal to numeric data

---
+++ flip.py
@@ -0,0 +1,61 @@
+d = {
+'school': {
+    0: 'GP', 'GP': 0, 
+    1: 'MS', 'MS': 1},
+'sex': {
+    0: 'F', 'F': 0, 
+    1: 'M', 'M': 1},
+'address': {
+    0: 'R', 'R': 0, 
+    1: 'U', 'U': 1},
+'famsize': {
+    0: 'GT3', 'GT3': 0, 
+    1: 'LE3', 'LE3': 1},
+'Pstatus': {
+    0: 'A', 'A': 0, 
+    1: 'T', 'T': 1},
+'Mjob': {
+    0: 'at_home', 'at_home': 0, 
+    1: 'health', 'health': 1,
+    2: 'other', 'other': 2,
+    3: 'services', 'services': 3,
+    4: 'teacher', 'teacher': 4}, 
+'Fjob': {
+    0: 'at_home', 'at_home': 0,
+    1: 'health', 'health': 1,
+    2: 'other', 'other': 2,
+    3: 'services', 'services': 3,
+    4: 'teacher', 'teacher': 4},
+'reason': {
+    0: 'course', 'course': 0,
+    1: 'home', 'home': 1,
+    2: 'other', 'other': 2,
+    3: 'reputation', 'reputation': 3},
+'guardian': {
+    0: 'father', 'father': 0,
+    1: 'mother', 'mother': 1,
+    2: 'other', 'other': 2},
+'schoolsup': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1},
+'famsup': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1},
+'paid': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1},
+'activities': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1},
+'nursery': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1},
+'higher': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1},
+'internet': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1},
+'romantic': {
+    0: 'no', 'no': 0, 
+    1: 'yes', 'yes': 1}}
